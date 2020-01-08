255d30748beacc239d25af5a1c1462d29933479c
Lucas Roche <lucas.roche@hetic.net>
2020-01-08 13:36:20 +0100

    nothing

--- src/js/script.js
+++ src/js/script.js
@@ -1,23 +1,23 @@
-oxo.inputs.listenKeyOnce('enter', function() {
-  oxo.screens.loadScreen('game', function() {
+oxo.inputs.listenKeyOnce("enter", function() {
+  oxo.screens.loadScreen("game", function() {
     game();
   });
 });
 
 function endFunction() {
-  oxo.inputs.listenKeyOnce('enter', function() {
-    oxo.screens.loadScreen('game', function() {
+  oxo.inputs.listenKeyOnce("enter", function() {
+    oxo.screens.loadScreen("game", function() {
       game();
     });
   });
 }
 
 function game() {
-  const $originalTrees = document.querySelector('.game-area__trees');
-  const $gameArea = document.querySelector('.game-area');
-  const $brontis = document.querySelector('.brontis');
-  const $squeeze = document.querySelector('.squeeze');
-  const $life = document.querySelector('.game-area__life');
+  const $originalTrees = document.querySelector(".game-area__trees");
+  const $gameArea = document.querySelector(".game-area");
+  const $brontis = document.querySelector(".brontis");
+  const $squeeze = document.querySelector(".squeeze");
+  const $life = document.querySelector(".game-area__life");
 
   let apparitionCrabTemplate;
   let appartionTreeTemplate;
@@ -42,8 +42,8 @@ function game() {
     addSqueez();
     brontisCatchSqueeze();
 
-    $originalTrees.classList.add('is-moving');
-    $life.innerHTML = 'Life : ' + life;
+    $originalTrees.classList.add("is-moving");
+    $life.innerHTML = "Life : " + life;
   })();
 
   function itemsApparition() {
@@ -90,37 +90,37 @@ function game() {
   }
 
   function addSqueez() {
-    $squeeze.classList.add('is-visible');
+    $squeeze.classList.add("is-visible");
   }
 
   function addBrontis() {
-    $brontis.classList.add('is-visible');
+    $brontis.classList.add("is-visible");
   }
   function moveBrontis() {
     oxo.inputs.listenArrowKeys(function(key) {
       switch (key) {
-        case 'up':
+        case "up":
           brontisIsJunping();
           break;
-        case 'down':
-          if ($brontis.classList.contains('is-jumping')) {
-            $brontis.classList.remove('is-jumping');
+        case "down":
+          if ($brontis.classList.contains("is-jumping")) {
+            $brontis.classList.remove("is-jumping");
           } else {
             brontisIsBendingDown();
           }
           break;
-        case 'right':
-          oxo.animation.move($brontis, 'right', 4, true);
+        case "right":
+          oxo.animation.move($brontis, "right", 6, true);
           break;
-        case 'left':
-          oxo.animation.move($brontis, 'left', 4, true);
+        case "left":
+          oxo.animation.move($brontis, "left", 6, true);
           break;
       }
     });
-    window.addEventListener('keyup', function(key) {
+    window.addEventListener("keyup", function(key) {
       switch (key.keyCode) {
         case 40:
-          $brontis.classList.remove('is-bending-down');
+          $brontis.classList.remove("is-bending-down");
       }
     });
   }
@@ -131,10 +131,10 @@ function game() {
         if (life > 0) {
           life--;
           brontisIsInvincible = true;
-          $brontis.classList.add('is-flashing');
-          $life.innerHTML = 'Life : ' + life;
+          $brontis.classList.add("is-flashing");
+          $life.innerHTML = "Life : " + life;
           setTimeout(function() {
-            $brontis.classList.remove('is-flashing');
+            $brontis.classList.remove("is-flashing");
             brontisIsInvincible = false;
           }, 3000);
         } else {
@@ -146,32 +146,32 @@ function game() {
 
   function brontisCatchSqueeze() {
     oxo.elements.onCollisionWithElement($brontis, $squeeze, function() {
-      console.log('FIGHT');
+      console.log("FIGHT");
       clearAllIntervalAndTimeout();
       freezeTrees();
     });
   }
 
   function brontisIsJunping() {
-    if (!$brontis.classList.contains('is-jumping')) {
-      $brontis.classList.add('is-jumping');
+    if (!$brontis.classList.contains("is-jumping")) {
+      $brontis.classList.add("is-jumping");
       setTimeout(function() {
-        $brontis.classList.remove('is-jumping');
+        $brontis.classList.remove("is-jumping");
       }, 1000);
     }
   }
 
   function brontisIsBendingDown() {
-    if (!$brontis.classList.add('is-bending-down')) {
-      $brontis.classList.add('is-bending-down');
+    if (!$brontis.classList.add("is-bending-down")) {
+      $brontis.classList.add("is-bending-down");
     }
   }
   function brontisIsDead() {
-    $brontis.classList.add('is-dead');
+    $brontis.classList.add("is-dead");
     brontisIsAlive = false;
     clearAllIntervalAndTimeout();
     setTimeout(function() {
-      oxo.screens.loadScreen('end', function() {
+      oxo.screens.loadScreen("end", function() {
         endFunction();
       });
     }, 2000);
@@ -186,11 +186,11 @@ function game() {
   }
 
   function appearRandomCrab() {
-    const $crab = document.createElement('div');
-    $crab.classList.add('crab');
+    const $crab = document.createElement("div");
+    $crab.classList.add("crab");
     $gameArea.appendChild($crab);
     setInterval(function() {
-      oxo.animation.move($crab, 'left', 1, true);
+      oxo.animation.move($crab, "left", 1, true);
     }, 10);
     oxo.elements.onLeaveScreenOnce(
       $crab,
@@ -203,12 +203,12 @@ function game() {
   }
 
   function appearRandomTree() {
-    const $tree = document.createElement('div');
-    $tree.classList.add('trees', 'tree__random');
-    $tree.style.width = getRandomNumber(150, 350) + 'px';
+    const $tree = document.createElement("div");
+    $tree.classList.add("trees", "tree__random");
+    $tree.style.width = getRandomNumber(150, 350) + "px";
     $gameArea.appendChild($tree);
     itemsMovingTemplate = setInterval(function() {
-      oxo.animation.move($tree, 'left', 1, true);
+      oxo.animation.move($tree, "left", 1, true);
     }, 30);
     oxo.elements.onLeaveScreenOnce(
       $tree,
@@ -220,21 +220,21 @@ function game() {
   }
 
   function freezeTrees() {
-    document.querySelectorAll('.tree__random').forEach(function(tree) {
+    document.querySelectorAll(".tree__random").forEach(function(tree) {
       var position = oxo.animation.getPosition(tree);
-      tree.style.transform = 'none';
-      tree.style.left = 1290 + position.x + 'px';
+      tree.style.transform = "none";
+      tree.style.left = 1290 + position.x + "px";
     });
   }
 
   function appearRandomBall() {
-    const $ball = document.createElement('div');
-    $ball.classList.add('ball');
+    const $ball = document.createElement("div");
+    $ball.classList.add("ball");
     $gameArea.appendChild($ball);
     let positionX = $ball.offsetLeft;
     setInterval(function() {
       positionX -= 1;
-      $ball.style.left = positionX + 'px';
+      $ball.style.left = positionX + "px";
     }, 6);
     oxo.elements.onLeaveScreenOnce(
       $ball,
@@ -247,11 +247,11 @@ function game() {
   }
 
   function appearRandomDog() {
-    const $dog = document.createElement('div');
-    $dog.classList.add('dog');
+    const $dog = document.createElement("div");
+    $dog.classList.add("dog");
     $gameArea.appendChild($dog);
     setInterval(function() {
-      oxo.animation.move($dog, 'left', 1, true);
+      oxo.animation.move($dog, "left", 1, true);
     }, 4);
     oxo.elements.onLeaveScreenOnce(
       $dog,
@@ -264,8 +264,8 @@ function game() {
   }
 
   function appearRandomBoat() {
-    const $boat = document.createElement('div');
-    $boat.classList.add('boat');
+    const $boat = document.createElement("div");
+    $boat.classList.add("boat");
     $gameArea.appendChild($boat);
     oxo.elements.onLeaveScreenOnce(
       $boat,
